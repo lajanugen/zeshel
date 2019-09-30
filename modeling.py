584f15d5b0ae285df7dcd1a3e99ffae2203984dc
Lajanugen Logeswaran <llajan@deeplearn20.eecs.umich.edu>
2019-09-30 00:15:06 -0400

    License

--- modeling.py
+++ modeling.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The main BERT model and related functions."""
+"""Model and related functions."""
 
 from __future__ import absolute_import
 from __future__ import division
